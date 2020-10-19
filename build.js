5b4c62c01501444923b2549ee2408f4a2d007cd8
Soumya Kanti Sar <soumyakantisar@gmail.com>
2020-10-19 20:16:08 +0530

    Add files via upload

--- build.js
+++ build.js
@@ -6,14 +6,16 @@ const LIST = {};
 function list(dir) {
     let _files = [];
     fs.readdirSync(dir).forEach(file => {
-        let path = dir + file;
-        let isDirectory = fs.lstatSync(path + '/').isDirectory();
-        let isFile = fs.lstatSync(path).isFile();
-        if ((isFile || isDirectory))
-            _files.push({ file, type: isDirectory ? 'dir' : 'file', ext: isFile ? file.split('.').pop() : '' });
-        if (isDirectory) list(dir + file + '/');
+        if (file.substr(0, 1) != '.') {
+            let path = dir + file;
+            let isDirectory = fs.lstatSync(path + '/').isDirectory();
+            let isFile = fs.lstatSync(path).isFile();
+            if ((isFile || isDirectory))
+                _files.push({ file, type: isDirectory ? 'dir' : 'file', ext: isFile ? file.split('.').pop() : '' });
+            if (isDirectory) list(dir + file + '/');
+        }
     });
-    LIST[dir.substr(1, dir.length)] = _files;
+    LIST[dir.substr(1)] = _files;
 }
 
 list('./');
