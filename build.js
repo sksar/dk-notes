a0d1bd493a16cfbf4af55cf7b0072573aaa22e2e
Soumya Kanti Sar <soumyakantisar@gmail.com>
2020-10-19 20:08:43 +0530

    Add files via upload

---
+++ build.js
@@ -0,0 +1,22 @@
+const { FILE } = require('dns');
+const fs = require('fs');
+
+const LIST = {};
+
+function list(dir) {
+    let _files = [];
+    fs.readdirSync(dir).forEach(file => {
+        let path = dir + file;
+        let isDirectory = fs.lstatSync(path + '/').isDirectory();
+        let isFile = fs.lstatSync(path).isFile();
+        if ((isFile || isDirectory))
+            _files.push({ file, type: isDirectory ? 'dir' : 'file', ext: isFile ? file.split('.').pop() : '' });
+        if (isDirectory) list(dir + file + '/');
+    });
+    LIST[dir.substr(1, dir.length)] = _files;
+}
+
+list('./');
+
+LIST['/'] = LIST['/'].filter(f => f.type == 'dir');
+fs.writeFileSync('files.json', JSON.stringify(LIST));
